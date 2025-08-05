1f83f732bd292e39a316801c32360a5edc5de19d
Jakson Fritzen <jaksoneufrasio@gmail.com>
2025-08-04 21:47:31 -0300

    feat: Conexão com o banco de dados (aula 04/08)

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'aws-0-sa-east-1.pooler.supabase.com',
+      port: +'5432',
+      username: 'postgres.dwxzvwhkttmmjefhxrfv',
+      password: '2320050323Jk!',
+      database: 'postgres',
+    }),
+  ],
+})
+export class AppModule { }
