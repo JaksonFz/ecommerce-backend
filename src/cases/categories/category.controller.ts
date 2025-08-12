5b189c6cc5e7f21bd78fa63324c9052e58764173
Jakson Fritzen <jaksoneufrasio@gmail.com>
2025-08-11 21:59:30 -0300

    feat: Parte 1 da implementação do case Category

---
+++ src/cases/categories/category.controller.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+
+import { CategoryService } from "./category.service";
+import { Category } from "./category.entity";
+import { Controller, Get, ParseUUIDPipe } from "@nestjs/common";
+
+@Controller('categories')
+export class CategoryController {
+
+    constructor(private readonly service: CategoryService) { }
+
+    @Get()
+    findAll(): Promise<Category[]> {
+        return this.service.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id', ParseUUIDPipe) id: string): Promise<Category[]> {
+
+    }
+}
